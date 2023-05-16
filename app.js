aa989476015b9c2f92bbfe2d7a9f1536410d1591
hsp2810 <hsp2810@gmail.com>
2023-05-16 12:57:05 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+function updateMap() {
+	console.log("Updating data in every 2 seconds....");
+	fetch("./data.json")
+		.then((response) => response.json())
+		.then((rsp) => {
+			console.log(rsp.data);
+			rsp.data.forEach((element) => {
+				latitude = element.latitude;
+				longitude = element.longitude;
+
+				recovered = element.recovered;
+
+				cases = element.infected;
+
+				if (recovered > 255) {
+					color = "rgb(0, 255, 0)";
+				} else {
+					color = `rgb(0, ${cases}, 0)`;
+				}
+
+				if (cases > 255) {
+					color = "rgb(255, 0, 0)";
+				} else {
+					color = `rgb(${cases}, 0, 0)`;
+				}
+
+				// Adding marker to the map
+				new mapboxgl.Marker({
+					draggable: false,
+					color: color,
+				})
+					.setLngLat([longitude, latitude])
+					.addTo(map);
+			});
+		});
+}
+let interval = 2000;
+setInterval(updateMap, interval);
